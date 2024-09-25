d8de13de9a83c8bd8ebc7ad8f9238560c7d94140
Hi im Andre <handrelukas@gmail.com>
2024-09-24 23:05:59 -0300

    "Change Google to Yahoo Finance (for more stability)"

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-link = "https://www.google.com/search?q=dolar&oq=dolar"
+link = "https://finance.yahoo.com/quote/USDBRL=X/?guccounter=1"
 headers = {
   "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Mobile Safari/537.36"
 }
@@ -10,7 +10,7 @@ rqst = requests.get(link, headers=headers)
 
 site = BeautifulSoup(rqst.text, "html.parser")
 
-site_valor = site.find("span" , class_="DFlfde SwHCTb")
+site_valor = site.find("fin-streamer", class_= "livePrice yf-1i5aalm")
 if site_valor:
   print(f"Valor do dolar: {site_valor.text}")
 else:
