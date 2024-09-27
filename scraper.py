bbcb718212e1c06a7fb5b0ce5b6412de58cb077f
Hi im Andre <handrelukas@gmail.com>
2024-09-26 22:29:55 -0300

    Fix: Data formatting issue in CSV file.

--- scraper.py
+++ scraper.py
@@ -19,16 +19,17 @@ while True:
         rqst = requests.get(link, headers=headers)
         site = BeautifulSoup(rqst.text, "html.parser")
  
-        site_valor = site.find("fin-streamer", class_="livePrice yf-1i5aalm")
+        site_valor = site.find("fin-streamer", class_="livePrice yf-1tejb6")
         
         if site_valor:
             data_hora = datetime.now(tz=None).strftime("%x %H:%M:%S")
             print(f"Dollar price in {data_hora} -- {site_valor.text}")
             
             nova_linha= {"Data e hora":data_hora, "Moeda": "Dolar","Valor": site_valor.text}
-            df = pd.concat([df,pd.DataFrame([nova_linha])])
+            nova_linha_df= pd.DataFrame([nova_linha])
+            df = pd.concat([df, nova_linha_df], ignore_index=True)
 
-            data_csv = df.to_csv("daily_dollar")
+            data_csv = df.to_csv("daily_dollar.csv", index=False)
         
         else:
             print("Não foi possível encontrar o valor do dólar.")
