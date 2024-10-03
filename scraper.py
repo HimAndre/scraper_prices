9d3a9c90d30fc200542d6925570f42234e4e3064
Hi im Andre <handrelukas@gmail.com>
2024-10-03 15:44:35 -0300

    fix: Corrigido bug que impedia a abertura do código via CMD

--- scraper.py
+++ scraper.py
@@ -5,8 +5,8 @@ import pandas as pd
 from datetime import datetime
 
 
-token = ''
-chat_id = ""
+token = 'seu_token'
+chat_id = "seu_chat_id"
 df = pd.DataFrame(columns=["Data e hora","Valor"])
 
 
@@ -16,7 +16,7 @@ def mensage_telegram(token,chat_id,mensagem):
     response = requests.post(url, data=data)
     return response
   
-               
+# Usando nesse codigo um site para scrappar o valor da racao do meu pet              
 link = "https://www.americanpet.com.br/7898604432968-racao-seca-nd-prime-canine-adultos-maxi-cordeiro-10-1kg/p"
 headers = {
     "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Mobile Safari/537.36"
@@ -26,19 +26,32 @@ headers = {
 while True:
     try:
         
+        print("Acessando a página...")
         rqst = requests.get(link, headers=headers)
+        rqst.raise_for_status() # Retorna um erro se houver um problema de requisiçao
+        print("Página acessada")
+        
         site = BeautifulSoup(rqst.text, "html.parser")
-                
+        
+        # Captura valor do produto        
         site_valor = site.find("p", class_="amepettatix-store-5-x-priceSubscription--withDiscount")
+        
         if site_valor:
             data_hora = datetime.now(tz=None).strftime("%x %H:%M:%S")
-            print(f"Dollar price in {data_hora} -- {site_valor.text}")
+            print(f"Valor do produto em {data_hora} -- {site_valor.text}")
             
             nova_linha= {"Data e hora":data_hora,"Valor": site_valor.text}
             nova_linha_df= pd.DataFrame([nova_linha])
             df = pd.concat([df, nova_linha_df], ignore_index=True)
-            data_csv = df.to_csv("daily_valor_racao.csv", index=False)
+            
+            df.to_csv(r"C:\Users\handr\Documents\daily_valor_racao.csv", index=False)
+            
+            # Salvar um arqv CSV
+            # O codigo nao estava executando via cmd, pois o arqv csv nao erra acessivel pelo cmd
+            # Ao salver usando um caminho absoluto. Independente de onde estiver sendo executado pelo cmd 
+            df.to_csv(r"C:\Users\handr\Documents\daily_valor_racao.csv", index=False)
 
+            # Envia mensagem para o Telegram
             mensagem = f"Valor da ração em {data_hora} é {site_valor.text}"
             mensage_telegram(token, chat_id, mensagem)
             
@@ -46,8 +59,11 @@ while True:
         else:
             print("Não foi possível encontrar o valor do produto.")
 
-    except KeyboardInterrupt:
-        print("Coleta interrompida pelo usuário")
-
-    
-    time.sleep(21600)
+    # Caso ocorra um erro de solicitaçao HTTP.
+    except requests.exceptions.RequestException as e:
+        print(f"Erro ao acessar a página: {e}")  
+    except Exception as e:
+        print(f"Ocorreu um erro {e}")
+    finally:
+        print("Esperando 6 horas para a próxima verificação")
+        time.sleep(21600)
